3561d2b574938b254a6836f657747d4bac40704a
Sorin Velican <sorin.velican@endava.com>
2020-12-07 17:07:15 +0200

    fix

--- src/test/java/com/endava/testing/tests/GrapeRipenessTest.java
+++ src/test/java/com/endava/testing/tests/GrapeRipenessTest.java
@@ -1,26 +1,29 @@
 package com.endava.testing.tests;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 @RunWith(SerenityRunner.class)
 public class GrapeRipenessTest {
 
-    public static final String GRAPE_NAME = "new_grape2";
-    public static final String GRAPE_QUANTITY = "16.0";
-    public static final String GRAPE_AGE = "66";
-    public static final String GRAPE_RIPENESS = "89.0";
-    public static final String APP_URL = "https://endavawineapp.azurewebsites.net";
+    public final Logger LOGGER = Logger.getLogger(getClass().getName());
+
+
+    public static final String APP_URL = "https://wineappui.azurewebsites.net/";
+
+    public static final By MENU = By.cssSelector("nav");
+    public static final By WINES_TYPES = By.cssSelector("li:nth-child(1)");
+    public static final By WINES_VOLUMES = By.cssSelector("li:nth-child(2)");
 
     @Managed(driver = "chrome")
     WebDriver driver;
@@ -28,37 +31,50 @@ public class GrapeRipenessTest {
     @Before
     public void setUp() {
 
-        Map<String, Object> quantity = new HashMap<>();
-        quantity.put("value", GRAPE_QUANTITY);
-        quantity.put("unit", "rows");
-
-        Map<String, Object> bodyMap = new HashMap<>();
-        bodyMap.put("name", GRAPE_NAME);
-        bodyMap.put("quantity", quantity);
-        bodyMap.put("age", GRAPE_AGE);
-        bodyMap.put("ripeness", GRAPE_RIPENESS);
-
-
-        Response response = SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .body(bodyMap)
-                .when()
-                .post(APP_URL + "/grapes");
-
-        System.out.println(response.prettyPrint());
-
+        driver.get(APP_URL);
     }
 
     @Test
     public void testGrapeRipeness() {
-        driver.get(APP_URL);
+
+        sleep(2);
+
+        selectFromMenu("Wines");
+
+        sleep(2);
+
+        assertThat(getWinesNumber(), greaterThanOrEqualTo(6));
+        LOGGER.info("Wines: " + getWinesNumber());
+
+        assertThat(getVolume(), greaterThanOrEqualTo(2696));
+        LOGGER.info("Volume: " + getVolume());
+    }
+
+    private int getVolume() {
+
+        String text = driver.findElement(WINES_VOLUMES).getText();
+
+        return Integer.parseInt(text.replaceAll("\\D+", ""));
+    }
+
+    private int getWinesNumber() {
+
+        String text = driver.findElement(WINES_TYPES).getText();
+
+        return Integer.parseInt(text.replaceAll("\\D+", ""));
+    }
+
+    private void selectFromMenu(String linkText) {
+
+        driver.findElement(MENU).findElement(By.linkText(linkText)).click();
+    }
+
+    private void sleep(int seconds) {
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
     }
 }
