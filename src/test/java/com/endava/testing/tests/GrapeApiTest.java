daeab3c713ee0753bb7ea64ad9f7808917c46953
velicans <velicans1@yahoo.com>
2020-12-09 18:06:57 +0200

    Update GrapeApiTest.java

--- src/test/java/com/endava/testing/tests/GrapeApiTest.java
+++ src/test/java/com/endava/testing/tests/GrapeApiTest.java
@@ -26,7 +26,7 @@ public class GrapeApiTest {
 
     public Response response;
 
-    public static final String GRAPE_NAME = "sorinTest4"; // schimba valoarea pentru a avea un nou tip de strugure
+    public static final String GRAPE_NAME = "sorinTest5"; // schimba valoarea pentru a avea un nou tip de strugure
     public static final float GRAPE_QUANTITY = 12;
     public static final int GRAPE_AGE = 5;
     public static final float GRAPE_RIPENESS = 99; // daca valoarea este pe 87.0 o sa avem butonul "pick & crush grapes"
